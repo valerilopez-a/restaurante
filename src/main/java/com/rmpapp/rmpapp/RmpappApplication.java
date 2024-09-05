2c78f39283a82413c9aafbc454e96b61c9ed775e
valerilopez-a <valeria.lopezest@gmail.com>
2024-09-05 17:51:52 -0500

    Proyecto Base

---
+++ src/main/java/com/rmpapp/rmpapp/RmpappApplication.java
@@ -0,0 +1,13 @@
+package com.rmpapp.rmpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmpappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmpappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmpapp/rmpapp/RmpappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmpapp.rmpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmpappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
